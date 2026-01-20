96c56f17f65992a9ce57193f0e965468d59bd7f7
Dodosau <dorian.sauzede@gmail.com>
2026-01-20 13:27:50 -0500

    Create calendar.js

---
+++ calendar/calendar.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="calendar.css">
+</head>
+
+<body>
+  <div id="calendar"></div>
+  <script src="calendar.js"></script>
+</body>
+</html>
